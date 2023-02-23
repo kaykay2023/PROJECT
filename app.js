9c3280b28dee96b2ea333ff53858194b20e11903
olu johnson <galaxybrightt@gmail.com>
2023-02-22 23:37:39 -0500

    commit

---
+++ app.js
@@ -0,0 +1,43 @@
+//Display images
+const Image = [
+  {
+    src: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/50f95eb5-7c8c-4f06-9ccf-ba936069debf/air-jordan-13-retro-big-kids-shoes-3Bbs9J.png",
+    name: "jordan retro",
+  },
+  {
+    src: "https://static.nike.com/a/images/t_PDP_864_v1,f_auto,q_auto:eco/852c5323-cf29-435d-919d-2936c4bce051/air-jordan-5-x-dj-khaled-mens-shoes-F3jrrH.png",
+    name: "jordan5-dj khaled",
+  },
+
+  {
+    src: "https://houseofheat.co/app/uploads/2022/08/air-jordan-13-black-flint-dj5982-060-release-date-1.jpg",
+    name: "Jordan Black flint",
+  },
+
+  {
+    src: "https://assets.hermes.com/is/image/hermesproduct/freestyle-sneaker--222920ZH90-worn-1-0-0-800-800_g.jpg",
+    name: "Hermes freestyle sneaker",
+  },
+
+  {
+    src: "https://assets.hermes.com/is/image/hermesproduct/freestyle-sneaker--222914ZHBK-worn-1-0-0-800-800_g.jpg",
+    name: "Hermes",
+  },
+];
+
+let currentPlayer = 1;
+let player1Score = 0;
+let player2Score = 0;
+
+//  currentImage = Math.floor(Math.random() * images.length);
+//   document.getElementById('image').src = currentImage;
+
+// Add event listner
+
+// Display the new image
+
+//get player results
+
+//compare answers
+
+// Add event listner to button
